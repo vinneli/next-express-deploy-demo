7cd1eecc48917c78c3aa483b3619a16c699c6349
libing <icoon@qq.com>
2019-09-16 16:20:52 +0800

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'next-express-demo',
+    script: './bin/server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    log_date_format: "YYYY-MM-DD HH:mm Z",
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_dev: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root1',
+      host : '192.168.126.25',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/usr/local/sandai/next-express-demo',
+      'post-deploy' : 'npm install && npm run pm2:start',
+      env: {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
